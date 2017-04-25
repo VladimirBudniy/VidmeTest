55ebb2d13c411000f80b78d1577c59a84e058b34
Vladimir Budniy <vladimir.budniy@Antons-MacBook-Air.local>
2017-04-25 09:18:23 +0300

    create project

---
+++ VidMe/VidMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VidMe
+//
+//  Created by Vladimir Budniy on 4/25/17.
+//  Copyright © 2017 Vladimir Budniy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
