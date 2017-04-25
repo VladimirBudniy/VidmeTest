e457fc2dd8bd88c4724dde29798536c55d96ac0e
Vladimir Budniy <vladimir.budniy@Antons-MacBook-Air.local>
2017-04-25 12:31:49 +0300

    file Constants.swift was create

---
+++ VidMe/Helper/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  VidMeTest
+//
+//  Created by Nikola Andriiev on 24.11.16.
+//  Copyright © 2016 Andriiev.Mykola. All rights reserved.
+//
+
+import Foundation
+
+struct URLPaths {
+    let featured = "https://api.vid.me/videos/featured"
+    let channels = "https://api.vid.me/videos/new"
+    let channelItems = "https://api.vid.me/videos/following"
+}
+
+struct UsersPaths {
+    let create = "https://api.vid.me/auth/create"
+    let check = "https://api.vid.me/auth/check"
+    let delete = "https://api.vid.me/auth/delete"
+}
