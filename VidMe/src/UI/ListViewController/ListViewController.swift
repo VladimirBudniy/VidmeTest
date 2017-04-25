2bc78aa8e58190dadfad3a565cc73ac6b85a342b
Vladimir Budniy <vladimir.budniy@Antons-MacBook-Air.local>
2017-04-25 12:32:46 +0300

    ListViewController fixed

--- VidMe/src/UI/ListViewController/ListViewController.swift
+++ VidMe/src/UI/ListViewController/ListViewController.swift
@@ -8,14 +8,30 @@
 
 import UIKit
 
-class ListViewController: UITableViewController {
-
+class ListViewController: UITableViewController, AlertViewController {
+    
+    let alertViewController = UIAlertController()
     let identifier = String(describing: ListViewCell.self)
+    var videos = [Video]()
+
+    // MARK: - View LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.settingTableVie()
+    
+        
+        
+        
+        loadVideos(URLPaths().featured, videosBlock: self.reloadViewWith, errorBlock: self.loadError)
+    
+    
+    
+    
+    
+    
+    
     }
     
     // MARK: - Private
@@ -23,6 +39,25 @@ class ListViewController: UITableViewController {
     fileprivate func settingTableVie() {
         self.tableView.contentInset.top = 20
         self.addRefreshControl()
+    }
+    
+    private func showAlertController(title: String? = nil, message: String) {
+        self.present(self.alertViewController(title: title, message: message), animated: true, completion: nil)
+    }
+    
+    // MARK: - Blocks methods
+    
+    private func loadError(error: String) {
+        self.tableView?.refreshControl?.endRefreshing()
+        self.showAlertController(title: "", message: error)
+    }
+    
+    private func reloadViewWith(_ videos: [Video]) {
+        if videos.count != 0 {
+            self.videos.append(contentsOf: videos)
+//            self.tableView?.reloadData()
+//            self.tableView?.refreshControl?.endRefreshing()
+        }
     }
     
     // MARK: - UIRefreshControl
@@ -40,7 +75,7 @@ class ListViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 3
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
